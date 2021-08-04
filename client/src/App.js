eb3f8945e158a2803887856cb245006adf85ef1c
Andrew Miller <jazznerd206@icloud.com>
2021-08-04 09:02:52 -0700

    trying again

--- client/src/App.js
+++ client/src/App.js
@@ -6,57 +6,57 @@ function App() {
     {
       index: 0,
       name: 'beepboop',
-      location: '../assets/audio/beepboop.wav',
+      location: '../audio/beepboop.wav',
       icon: "fas fa-bell fa-5x"
     },
     {
       index: 1,
       name: 'heartbeat',
-      location: '../assets/audio/heartbeat.wav',
+      location: '../audio/heartbeat.wav',
       icon: "fas fa-heartbeat fa-5x"
     },
     {
       index: 2,
       name: 'rocket',
-      location: '../assets/audio/rocket.wav',
+      location: '../audio/rocket.wav',
       icon: "fas fa-rocket fa-5x"
     },
     {
       index: 3,
       name: 'transition',
-      location: '../assets/audio/transition.wav',
+      location: '../audio/transition.wav',
       icon: "fas fa-arrow-circle-right fa-5x"
     },
     {
       index: 4,
       name: 'trumpet',
-      location: '../assets/audio/trumpet.wav',
+      location: '../audio/trumpet.wav',
       icon: "fas fa-chess-king fa-5x"
     },
     {
       index: 5,
       name: 'whoosh',
-      location: '../assets/audio/whoosh.wav',
+      location: '../audio/whoosh.wav',
       icon: "fas fa-wind fa-5x"
     },
     {
       index: 6,
       name: 'airhorn',
-      location: '../assets/audio/airhorn.mp3',
+      location: '../audio/airhorn.mp3',
       icon: "fas fa-bullhorn fa-5x"
     },
     {
       index: 7,
       name: 'alright',
-      location: '../assets/audio/alright.mp3',
-      image: "../assets/images/mm.jpeg",
+      location: '../audio/alright.mp3',
+      image: "../images/mm.jpeg",
       mm: true
     },
     {
       index: 8,
       name: 'whoawhoa',
-      location: '../assets/audio/whoawhoa.m4a',
-      image: "../assets/images/whoawhoa.png",
+      location: '../audio/whoawhoa.m4a',
+      image: "../images/whoawhoa.png",
       mm: true
     },
   ]
